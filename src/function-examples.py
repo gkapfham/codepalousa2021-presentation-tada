6ea5715e0c68e9cf61f7daa76da25c0820b01b50
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-19 15:55:06 -0400

    Add the factorial function as extra example in function-examples.py.

--- src/function-examples.py
+++ src/function-examples.py
@@ -4,6 +4,15 @@ def add_digits(digits: str) -> int:
         value += int(digit)
     return value
 
+def factorial(x: int) -> int:
+    if x == 1:
+        return 1
+    else:
+        return x*factorial(x-1)
+
 
 sum_digits = add_digits("123")
 print(sum_digits)
+
+factorial_value = factorial(3)
+print(factorial_value)
