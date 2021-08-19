d35e5fffb79114e150b20ebb4b2b96424523608e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2021-08-19 16:52:35 -0400

    Add remaining source code examples for first three functions in function-examples.py.

--- src/function-examples.py
+++ src/function-examples.py
@@ -1,8 +1,11 @@
+from typing import List
+
 def add_digits(digits: str) -> int:
     value = 0
     for digit in digits:
         value += int(digit)
     return value
+
 
 def factorial(x: int) -> int:
     if x == 1:
@@ -11,8 +14,25 @@ def factorial(x: int) -> int:
         return x*factorial(x-1)
 
 
+def is_subset(one: List, two: List) -> bool:
+    for element_one in one:
+        matched = False
+        for element_two in two:
+            if element_one == element_two:
+                matched = True
+                break
+        if not matched:
+            return False
+    return True
+
+
 sum_digits = add_digits("123")
 print(sum_digits)
 
 factorial_value = factorial(3)
 print(factorial_value)
+
+list_one = [1,2,3,4]
+list_two = [1,2,3,4,5]
+found_subset = is_subset(list_one, list_two)
+print(found_subset)
